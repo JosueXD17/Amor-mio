f69d82457a482efee130cffed06abd5a143dd7e6
JosueXD17 <j0961918421@gmail.com>
2025-02-03 17:31:33 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.getElementById("boton-si").addEventListener("click", function() {
-    alert("¡Genial! ¡Eres la mejor!");
-    document.body.style.backgroundImage = "url('fondo-amor.jpg')";
+    document.getElementById("boton-si").classList.add("animacion-si");
+    setTimeout(function() {
+        alert("¡Genial! ¡Eres la mejor!");
+    }, 2000);
 });
 
 document.getElementById("boton-no").addEventListener("click", function() {
-    alert("¡No importa! ¡Te amo de todos modos!");
+    document.getElementById("boton-no").classList.add("animacion-no");
+    setTimeout(function() {
+        alert("¡No importa! ¡Te amo de todos modos!");
+    }, 2000);
 });
