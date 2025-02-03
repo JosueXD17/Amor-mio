f6116a0a331d74bb10f55c41819f0b569fd788e8
JosueXD17 <j0961918421@gmail.com>
2025-02-03 17:07:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("boton-si").addEventListener("click", function() {
+    alert("¡Genial! ¡Eres la mejor!");
+    document.body.style.backgroundImage = "url('fondo-amor.jpg')";
+});
+
+document.getElementById("boton-no").addEventListener("click", function() {
+    alert("¡No importa! ¡Te amo de todos modos!");
+});
